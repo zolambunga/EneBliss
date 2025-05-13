b94bc1892d51e9f04462837d2010d9b91eab948d
Zola Mbunga <zolambunga@gmail.com>
2025-05-13 12:16:30 +0100

    Commited in 13-05-2025 12:15:58 by zombunga@DESKTOP-H5965OT
    added requirements.txt and enhanced development portability

--- notificacao/migrations/0002_notificacaoadmin_grupo_destinatario.py
+++ notificacao/migrations/0002_notificacaoadmin_grupo_destinatario.py
@@ -7,7 +7,8 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0024_user_foto_perfil'),
+       # ('auth', '0024_user_foto_perfil'),
+        ('auth', '0012_alter_user_first_name_max_length'),
         ('notificacao', '0001_initial'),
     ]
 
